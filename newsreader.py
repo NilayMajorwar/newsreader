82c72126ee309af96ab59f4e5cf464162f5eb859
Nilay Majorwar <majorwar@iitk.ac.in>
2019-01-14 17:42:51 +0530

    Add files via upload

---
+++ newsreader.py
@@ -0,0 +1,92 @@
+import tkinter as tk
+from datetime import datetime as dt
+import requests
+from bs4 import BeautifulSoup
+import time
+from threading import Thread
+
+# -----------------------------------------------------------------
+
+def getTopStory(url, categoryName):
+    data = requests.get(url)
+    soup = BeautifulSoup(data.text, features = 'xml')
+    item = soup.find('item')
+    try:
+        return categoryName, item.title.string
+    except AttributeError:
+        return categoryName, 'ERROR: NEWS ITEM NOT FOUND.'
+
+# -------------------------------------------------------------------
+
+data = requests.get("https://timesofindia.indiatimes.com/rss.cms")
+soup = BeautifulSoup(data.text, features = "html.parser")
+storyList = {}
+
+categoryList = soup.find(width = "640", border = "0").find_all('tr')
+masterList = []
+
+for category in categoryList:
+    linkTag = category.td.a
+    masterList.append((linkTag.string, linkTag.get('href')))
+
+for category in masterList:
+    story = getTopStory(category[1], category[0])
+    storyList[story[0]] = story[1]
+
+# -------------------------------------------------------------------
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.newsThread = None
+        self.statusLabel = None
+        self.categoryLabels = []
+        self.newsLabels = []
+        self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
+        self.createLabels()
+        self.createNewsLabels()
+        self.initiate()
+
+
+    def createLabels(self):
+        for i in range(20):
+            blank1 = tk.Label(self, text=' ')
+            blank2 = tk.Label(self, text=' ')
+            blank1.grid(row=0, column=0)
+            blank2.grid(row=2, column=0)
+            self.categoryLabels.append(tk.Label(self, text='\t'+masterList[i][0]+'   |   ', font='Manjari 18 bold'))
+            self.categoryLabels[i].grid(row=i+3, column=0, sticky=tk.E)
+            self.statusLabel = tk.Label(self, text='Newsreader', font='Manjari 15 italic')
+            self.statusLabel.grid(row=1, column=1)
+
+    def createNewsLabels(self):
+        for i in range(20):
+            self.newsLabels.append(tk.Label(self, text=storyList[masterList[i][0]], font='Manjari 18'))
+            self.newsLabels[i].grid(row=i+3, column=1, sticky=tk.W)
+
+    def updateLoop(self):
+        global storyList
+        while True:
+            time.sleep(60)
+            self.statusLabel.config(text='Getting updates...')
+            for i in range(20):
+                category = masterList[i]
+                newStory = getTopStory(category[1], category[0])
+                if newStory[1] != storyList[category[0]]:
+                    storyList[newStory[0]] = newStory[1]
+                    self.newsLabels[i].config(text=newStory[1])
+            statusStr = 'Last update: ' + dt.now().strftime('%H : %M : %S')
+            self.statusLabel.config(text=statusStr)
+
+    def initiate(self):
+        self.newsThread = Thread(target=self.updateLoop)
+        self.newsThread.daemon = True
+        self.newsThread.start()
+
+
+app = Application()
+app.master.geometry("1500x800")
+app.master.title('Newsreader v1.0')
+app.mainloop()
+
+
